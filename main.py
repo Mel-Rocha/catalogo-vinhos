a0ab48cf6f4d495d4fefb05fd7f10281cea60885
Mel-Rocha <rochamel73@gmail.com>
2024-03-23 23:29:37 -0300

    interactive doc ui customization

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
+from apps.docs import routes as docs_router
 from apps.auth.middlewares import AuthMiddleware
+from apps.docs.custom_openai import custom_openapi
 from apps.wine import routes as wine_router
 
 load_dotenv()
@@ -39,12 +41,15 @@ def create_application() -> FastAPI:
         allow_headers=["*"]
     )
 
+    application.include_router(docs_router.router, tags=['wine'])
     application.include_router(wine_router.router, prefix="/wine", tags=['wine'])
 
     return application
 
 
 app = create_application()
+
+app.openapi = lambda: custom_openapi(app)
 
 
 @app.on_event('startup')
