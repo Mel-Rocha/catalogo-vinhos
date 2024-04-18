4d92589b6a870d349725392809b697bdcb5d6d19
Mel-Rocha <rochamel73@gmail.com>
2024-04-17 22:39:36 -0300

    entering data with automation

--- automation/login.py
+++ automation/login.py
@@ -3,9 +3,7 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 service = Service(ChromeDriverManager().install())
 
